345dcef604fb20ad0a0b5093539c6d36ad5007b6
majidgoodini <majid.goodini@gmail.com>
2024-01-26 19:59:04 +0330

    .

--- sections/_e-learning/course/details/ElearningCourseDetailsInfo.tsx
+++ sections/_e-learning/course/details/ElearningCourseDetailsInfo.tsx
@@ -191,7 +191,7 @@ export default function ElearningCourseDetailsInfo({ course }: Props) {
             href={details?.filesLink as string}
             target='_blank'
           >
-            دانلود فایل‌‌های دوره
+            دانلود فایل‌‌های ضمیمه
           </Button>
         )}
 
--- sections/_e-learning/view/EmbedGraphViews.tsx
+++ sections/_e-learning/view/EmbedGraphViews.tsx
@@ -1,4 +1,3 @@
-
 import { useRouter } from 'next/router';
 import {
     useGetApiCoursesByIdAGraphQuery,
@@ -10,15 +9,22 @@ import type {  UserLessonViewMinute } from 'libs/redux/services/karnama'
 
 
 function EmbedGraphView() {
+    const { query } = useRouter();
+    const courseId = Number(query.courseId);
+    const { data: course, isLoading: courseLoading, error: courseError } = useGetApiCoursesByIdQuery({ id: courseId });
+    const { data: graph, isLoading: graphLoading, error: graphError } = useGetApiCoursesByIdAGraphQuery({ id: courseId, uuid: query.uuid as string });
 
-    const { query } = useRouter()
-    const { data: course, isLoading: courseLoading } = useGetApiCoursesByIdQuery({ id: Number(query.courseId) })
-    const { data: graph, isLoading: graphLoading } = useGetApiCoursesByIdAGraphQuery({ id: Number(query.courseId), uuid: query.uuid as string })
+    if (courseError || graphError) {
+        return <div>Error: Failed to fetch data</div>;
+    }
 
-    return (<>
-        {course?.sections?.map((section) => (
-            <ElearningCourseDetailsLessonList section={section} graph={graph as UserLessonViewMinute[]} canPlay={false} />
-        ))}
-    </>)
+    return (
+        <>
+            {course?.sections?.map((section) => (
+                <ElearningCourseDetailsLessonList section={section} graph={graph ? graph as UserLessonViewMinute[] : []} canPlay={false} />
+            ))}
+            {!course?.sections && <div>No sections found.</div>}
+        </>
+    );
 }
 export default EmbedGraphView
